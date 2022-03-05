223d644930bdc70ef10711001b36dde1cb166a17
Jared Bellamy <Bellamyjared@gmail.com>
2022-03-05 00:12:49 -0700

    added a temp landing page for pantry and recipe

--- pages/Pantry.js
+++ pages/Pantry.js
@@ -1,20 +1,73 @@
-import { StyleSheet, View } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
-import Icon from "../assets/icons/icon";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
+import NavBar from "../components/NavBar";
+import Header from "../components/Header";
+import ButtonBar from "../components/ButtonBar";
+import { GetCategory } from "../dbRequests/Category";
+// import { GetPantry } from "../dbRequests/Pantry";
 
 const Pantry = ({ navigation }) => {
-  let [fontsLoaded, error] = useFonts({
-    icomoon: require("../assets/fonts/icomoon.ttf"),
-  });
+  const [categories, setCategories] = useState([]);
+  // const [pantrylist, setPantrylist] = useState([]);
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+  useEffect(() => {
+    handleGetCategories();
+    // handleGetPantry();
+  }, []);
+
+  const handleGetCategories = async () => {
+    data = await GetCategory();
+    setCategories(data);
+  };
+  // const handleGetPantry = async () => {
+  //   data = await GetList();
+  //   setPantrylist(data);
+  // };
+
   return (
     <>
-      <View></View>
+      {/* ~~~~~~~~~~~~~~~~   HEADER  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.header}>
+        <Header
+          navigation={navigation}
+          title={["Pantry", 50]}
+          icons={[
+            ["edit", "Category"],
+            ["trash", ""],
+          ]}
+        />
+      </View>
+      {/* ~~~~~~~~~~~~~~~~   BODY  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.container}>
+        {categories.map((c) => (
+          <TouchableWithoutFeedback
+            onPress={() => {
+              console.log(c.category);
+            }}
+          >
+            <Text>{c.category}</Text>
+          </TouchableWithoutFeedback>
+        ))}
+      </View>
+      {/* ~~~~~~~~~~~~~~~~   BUTTONBAR  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.buttonBar}>
+        <ButtonBar
+          navigation={navigation}
+          buttonInfo={[
+            [
+              "plus_circle",
+              "Add_Items",
+              "passProps",
+              { OriginRoute: "pantry" },
+            ],
+          ]}
+        />
+      </View>
+      {/* ~~~~~~~~~~~~~~~~   NAVBAR  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.NavBar}>
+        <NavBar navigation={navigation} page={"pantry"} />
+      </View>
     </>
   );
 };
@@ -22,10 +75,26 @@ const Pantry = ({ navigation }) => {
 export default Pantry;
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 2,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  header: {
+    height: 100,
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+  },
+  text: { fontFamily: "Poppins-Regular", fontSize: 50 },
+
+  buttonBar: {
+    height: "12%",
+    paddingRight: "10%",
+    paddingTop: "5%",
+    alignItems: "flex-end",
+    backgroundColor: "#fff",
   },
   NavBar: {
     height: "10%",
--- pages/Recipe.js
+++ pages/Recipe.js
@@ -1,15 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Button, Text, View } from "react-native";
-import NavBar from "../components/NavBar";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
-const Recipe = ({ navigation }) => {
+import NavBar from "../components/NavBar";
+import Header from "../components/Header";
+import ButtonBar from "../components/ButtonBar";
+
+const List = ({ navigation }) => {
   return (
     <>
       {/* ~~~~~~~~~~~~~~~~   HEADER  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
-      {/* ~~~~~~~~~~~~~~~~   BODY  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
-      <View style={styles.main}>
-        <Text>test</Text>
+      <View style={styles.header}>
+        <Header
+          navigation={navigation}
+          title={["Recipe", 50]}
+          icons={[
+            ["edit", "Category"],
+            ["trash", ""],
+          ]}
+        />
       </View>
+      {/* ~~~~~~~~~~~~~~~~   BODY  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.container}></View>
+      {/* ~~~~~~~~~~~~~~~~   BUTTONBAR  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
+      <View style={styles.buttonBar}></View>
       {/* ~~~~~~~~~~~~~~~~   NAVBAR  ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       <View style={styles.NavBar}>
         <NavBar navigation={navigation} page={"recipe"} />
@@ -18,13 +31,29 @@ const Recipe = ({ navigation }) => {
   );
 };
 
-export default Recipe;
+export default List;
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 2,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  header: {
+    height: 100,
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+  },
+  text: { fontFamily: "Poppins-Regular", fontSize: 50 },
+
+  buttonBar: {
+    height: "12%",
+    paddingRight: "10%",
+    paddingTop: "5%",
+    alignItems: "flex-end",
+    backgroundColor: "#fff",
   },
   NavBar: {
     height: "10%",
